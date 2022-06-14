968763746e4e0a962b667168131cc8fdc85c9bc8
Xeniya Dobrogorskaya <xeniyad@Xeniyas-iMac.local>
2022-06-14 14:42:26 -0400

    React props

--- src/App.js
+++ src/App.js
@@ -4,84 +4,35 @@ function getTitle(title) {
   return title;
 }
 
-const list = [
-  {
-    id: "1",
-    name: "Mercury",
-    diameter: "3,031.67 mi",
-    moons: "none",
-    desc: "Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis. Up until 1965 it was thought that the same side of Mercury constantly faced the Sun.",
-    url: "mercury.jpg",
-  },
-  {
-    id: "2",
-    name: "Venus",
-    diameter: "7,521 mi",
-    moons: "none",
-    desc: "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Venus is the second largest terrestrial planet and is sometimes referred to as the Earth’s sister planet due the their similar size and mass.",
-    url: "img/venus.jpg",
-  },
-  {
-    id: "3",
-    name: "Earth",
-    diameter: "7,917.5 mi",
-    moons: "1",
-    desc: "Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.",
-    url: "img/earth.jpg",
-  },
-  {
-    id: "4",
-    name: "Mars",
-    diameter: "4,212 mi",
-    moons: "2",
-    desc: 'The fourth planet from the Sun and the second smallest planet in the solar system. Mars is often described as the "Red Planet" due to its reddish appearance. It\'s a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
-    url: "img/mars.jpg",
-  },
-];
+let List = (props) => {
+  return (
+    <ul>
+      {props.list.map(function (item) {
+        return <Item key={item.objectID} item={item} />;
+      })}
+    </ul>
+  );
+};
 
-let List = () => (
-  <ul>
-    {list.map(function (item) {
-      return (
-        <li key={item.id}>
-          <h2>{item.name}</h2>
-          <span>
-            <img src={item.url} alt={item.name} />
-          </span>
-          <p>{item.diameter}</p>
-          <p>{item.desc}</p>
-        </li>
-      );
-    })}
-  </ul>
-);
-
-// function List() {
-//   return (
-//     <ul>
-//       {list.map(function (item) {
-//         return (
-//           <li key={item.id}>
-//             <h2>{item.name}</h2>
-//             <span>
-//               <img src={item.url} alt={item.name} />
-//             </span>
-//             <p>{item.diameter}</p>
-//             <p>{item.desc}</p>
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <h2>{props.item.name}</h2>
+      <span>
+        <img src={props.item.url} alt={props.item.name} />
+      </span>
+      <p>{props.item.diameter}</p>
+      <p>{props.item.desc}</p>
+    </li>
+  );
+};
 function Search() {
   const handleChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
   };
 
   const handleMouseOver = (event) => {
-    console.log(event);
+    console.log(event.target.value);
   };
 
   return (
@@ -98,13 +49,47 @@ function Search() {
 }
 
 function App() {
-  //view part of react
+  const planets = [
+    {
+      objectID: 1,
+      name: "Mercury",
+      diameter: "3,031.67 mi",
+      moons: "none",
+      desc: "Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis. Up until 1965 it was thought that the same side of Mercury constantly faced the Sun.",
+      url: "mercury.jpg",
+    },
+    {
+      objectID: 2,
+      name: "Venus",
+      diameter: "7,521 mi",
+      moons: "none",
+      desc: "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Venus is the second largest terrestrial planet and is sometimes referred to as the Earth’s sister planet due the their similar size and mass.",
+      url: "img/venus.jpg",
+    },
+    {
+      objectID: 3,
+      name: "Earth",
+      diameter: "7,917.5 mi",
+      moons: "1",
+      desc: "Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.",
+      url: "img/earth.jpg",
+    },
+    {
+      objectID: 4,
+      name: "Mars",
+      diameter: "4,212 mi",
+      moons: "2",
+      desc: 'The fourth planet from the Sun and the second smallest planet in the solar system. Mars is often described as the "Red Planet" due to its reddish appearance. It\'s a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
+      url: "img/mars.jpg",
+    },
+  ];
+
   return (
     <div>
       <h1>Hello {getTitle("React")}</h1>
       <Search />
       <hr />
-      <List />
+      <List list={planets} />
     </div>
   );
 }
