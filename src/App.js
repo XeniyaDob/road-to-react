864743493e791dc96a54b0203e30cfc17bc972b0
Xeniya Dobrogorskaya <xeniyad@Xeniyas-iMac.local>
2022-06-29 14:57:42 -0400

    Lifting State and Filter Search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+//import img from "img";
 
 const App = () => {
   //console.log("App renders");
@@ -9,7 +10,7 @@ const App = () => {
       diameter: "3,031.67 mi",
       moons: "none",
       desc: "Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis. Up until 1965 it was thought that the same side of Mercury constantly faced the Sun.",
-      url: "mercury.jpg",
+      img: "earth.jpg",
     },
     {
       objectID: 2,
@@ -17,7 +18,7 @@ const App = () => {
       diameter: "7,521 mi",
       moons: "none",
       desc: "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Venus is the second largest terrestrial planet and is sometimes referred to as the Earth’s sister planet due the their similar size and mass.",
-      url: "img/venus.jpg",
+      img: "src/img/venus.jpg",
     },
     {
       objectID: 3,
@@ -25,7 +26,7 @@ const App = () => {
       diameter: "7,917.5 mi",
       moons: "1",
       desc: "Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.",
-      url: "img/earth.jpg",
+      img: "src/img/earth.jpg",
     },
     {
       objectID: 4,
@@ -33,19 +34,27 @@ const App = () => {
       diameter: "4,212 mi",
       moons: "2",
       desc: 'The fourth planet from the Sun and the second smallest planet in the solar system. Mars is often described as the "Red Planet" due to its reddish appearance. It\'s a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
-      url: "img/mars.jpg",
+      img: "./mars.jpg",
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState("");
+
   const handleSearch = (event) => {
     console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedPlanets = planets.filter((planet) =>
+    planet.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Hello {getTitle("React")}</h1>
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
       <hr />
-      <List list={planets} />
+      <List list={searchedPlanets} />
     </div>
   );
 };
@@ -55,13 +64,7 @@ const getTitle = (title) => {
 };
 
 const Search = (props) => {
-  //console.log("Search renders");
-  const [searchTerm, setSearchTerm] = React.useState("");
-  //console.log(`Rendering search with  searchTerm: ${searchTerm}`);
   const handleChange = (event) => {
-    //console.log(`Before setting searchTerm: ${searchTerm}`);
-    setSearchTerm(event.target.value);
-    //console.log(`After setting searchTerm: ${searchTerm}`);
     props.onSearch(event);
   };
 
@@ -70,7 +73,7 @@ const Search = (props) => {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
       <span>
-        Searching for <strong>{searchTerm}</strong>
+        Searching for <strong>{props.searchTerm}</strong>
       </span>
     </div>
   );
@@ -92,8 +95,9 @@ const Item = (props) => {
     <li key={props.item.objectID}>
       <h2>{props.item.name}</h2>
       <span>
-        <img src={props.item.url} alt={props.item.name} />
+        <img src={props.item.img} alt={props.item.name} />
       </span>
+      <p>{props.item.moons}</p>
       <p>{props.item.diameter}</p>
       <p>{props.item.desc}</p>
     </li>
