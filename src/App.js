366319753c20f37b116e2f525cad75253ceed0f9
Xeniya Dobrogorskaya <xeniya.dobrogorskaya@gmail.com>
2022-07-26 13:16:44 -0700

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -1,58 +1,83 @@
-import React, { useState, useEffect, useRef } from "react";
+import * as React from "react";
 
-const App = () => {
-  const initialPlanets = [
-    {
-      objectID: 1,
-      name: "Mercury",
-      diameter: "3,031.67 mi",
-      moons: "none",
-      desc: "Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis. Up until 1965 it was thought that the same side of Mercury constantly faced the Sun.",
-      img: "https://cdn.mos.cms.futurecdn.net/PFQ97KNjjTebMzenT3GeKd-1024-80.jpg.webp",
-    },
-    {
-      objectID: 2,
-      name: "Venus",
-      diameter: "7,521 mi",
-      moons: "none",
-      desc: "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Venus is the second largest terrestrial planet and is sometimes referred to as the Earth’s sister planet due the their similar size and mass.",
-      img: "https://cdn.mos.cms.futurecdn.net/oFF43BjXYUyyMTTJLFpeDE-1024-80.jpg.webp",
-    },
-    {
-      objectID: 3,
-      name: "Earth",
-      diameter: "7,917.5 mi",
-      moons: "1",
-      desc: "Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.",
-      img: "https://cdn.mos.cms.futurecdn.net/CSa5v4sqMRom94tfjr5scf-1024-80.jpg.webp",
-    },
-    {
-      objectID: 4,
-      name: "Mars",
-      diameter: "4,212 mi",
-      moons: "2",
-      desc: 'The fourth planet from the Sun and the second smallest planet in the solar system. Mars is often described as the "Red Planet" due to its reddish appearance. It\'s a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
-      img: "https://cdn.mos.cms.futurecdn.net/E95ZSoxdCKyWZWzkm2EyNe-1024-80.jpg.webp",
-    },
-  ];
+const initialPlanets = [
+  {
+    objectID: 1,
+    title: "Mercury",
+    diameter: "3,031.67 mi",
+    moons: "none",
+    desc:
+      "Mercury is the closest planet to the Sun. Due to its proximity, it's not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis. Up until 1965 it was thought that the same side of Mercury constantly faced the Sun.",
+    img:
+      "https://cdn.mos.cms.futurecdn.net/PFQ97KNjjTebMzenT3GeKd-1024-80.jpg.webp"
+  },
+  {
+    objectID: 2,
+    title: "Venus",
+    diameter: "7,521 mi",
+    moons: "none",
+    desc:
+      "Venus is the second planet from the Sun and is the second brightest object in the night sky after the Moon. Venus is the second largest terrestrial planet and is sometimes referred to as the Earth’s sister planet due the their similar size and mass.",
+    img:
+      "https://cdn.mos.cms.futurecdn.net/oFF43BjXYUyyMTTJLFpeDE-1024-80.jpg.webp"
+  },
+  {
+    objectID: 3,
+    title: "Earth",
+    diameter: "7,917.5 mi",
+    moons: "1",
+    desc:
+      "Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.",
+    img:
+      "https://cdn.mos.cms.futurecdn.net/CSa5v4sqMRom94tfjr5scf-1024-80.jpg.webp"
+  },
+  {
+    objectID: 4,
+    title: "Mars",
+    diameter: "4,212 mi",
+    moons: "2",
+    desc:
+      'The fourth planet from the Sun and the second smallest planet in the solar system. Mars is often described as the "Red Planet" due to its reddish appearance. It\'s a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.',
+    img:
+      "https://cdn.mos.cms.futurecdn.net/E95ZSoxdCKyWZWzkm2EyNe-1024-80.jpg.webp"
+  }
+];
 
-  const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = useState(
-      localStorage.getItem(key) || initialState
-    );
-    useEffect(() => {
-      localStorage.setItem(key, value);
-    }, [value, key]);
-
-    return [value, setValue];
-  };
-
-  const [searchTerm, setSearchTerm] = useSemiPersistentState(
-    "search",
-    "Planet"
+const getAsyncPlanets = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { planets: initialPlanets } }), 2000)
   );
 
-  const [planets, setPlanets] = useState(initialPlanets);
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "R");
+  //useSemiPersistentState( "search","R");-- this line of code means a lot,
+  //it searches "r" and rendering all planets that has this string,
+  // the book p.95-->
+  //Once you start the application again, you should see a delayed rendering of the list.
+  //The initial state for the stories is an empty array. After the App component is rendered,
+  //the side-effect hook runs once to fetch the asynchronous data. After
+  //resolving the promise and setting the data in the component’s state,
+  //the component renders again and displays the list of asynchronously loaded stories.
+
+  const [planets, setPlanets] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncPlanets().then((result) => {
+      setPlanets(result.data.planets);
+    });
+  }, []);
 
   const handleRemovePlanet = (item) => {
     const newPlanets = planets.filter(
@@ -63,29 +88,30 @@ const App = () => {
   };
 
   const handleSearch = (event) => {
-    console.log(event.target.value);
     setSearchTerm(event.target.value);
   };
 
   const searchedPlanets = planets.filter((planet) =>
-    planet.name.toLowerCase().includes(searchTerm.toLowerCase())
+    planet.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
-    <>
-      <h1>Hello {getTitle("Universe")}</h1>
+    <div>
+      <h1>Universe</h1>
+
       <InputWithLabel
         id="search"
         value={searchTerm}
         isFocused
         onInputChange={handleSearch}
       >
-        <strong>Search: </strong>
+        <strong>Search:</strong>
       </InputWithLabel>
 
       <hr />
+
       <List list={searchedPlanets} onRemoveItem={handleRemovePlanet} />
-    </>
+    </div>
   );
 };
 
@@ -95,11 +121,11 @@ const InputWithLabel = ({
   type = "text",
   onInputChange,
   isFocused,
-  children,
+  children
 }) => {
-  const inputRef = useRef();
+  const inputRef = React.useRef();
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (isFocused && inputRef.current) {
       inputRef.current.focus();
     }
@@ -110,8 +136,8 @@ const InputWithLabel = ({
       <label htmlFor={id}>{children}</label>
       &nbsp;
       <input
-        ref={inputRef}
         id={id}
+        ref={inputRef}
         type={type}
         value={value}
         onChange={onInputChange}
@@ -120,39 +146,29 @@ const InputWithLabel = ({
   );
 };
 
-const getTitle = (title) => {
-  return title;
-};
+const List = ({ list, onRemoveItem }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ))}
+  </ul>
+);
 
-const List = ({ list, onRemoveItem }) => {
-  //console.log("List renders");
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-        );
-      })}
-    </ul>
-  );
-};
+const Item = ({ item, onRemoveItem }) => (
+  <li key={item.objectID}>
+    <span>
+      <img src={item.img} alt={item.name} />
+    </span>
+    <h2>{item.title}</h2>
+    <p>{item.moons}</p>
+    <p>{item.diameter}</p>
+    <p>{item.desc}</p>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Remove
+      </button>
+    </span>
+  </li>
+);
 
-const Item = ({ item, onRemoveItem }) => {
-  return (
-    <li key={item.objectID}>
-      <h2>{item.name}</h2>
-      <span>
-        <img src={item.img} alt={item.name} />
-      </span>
-      <p>{item.moons}</p>
-      <p>{item.diameter}</p>
-      <p>{item.desc}</p>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
-          Remove
-        </button>
-      </span>
-    </li>
-  );
-};
 export default App;
