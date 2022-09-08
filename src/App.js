fd96311951f6bc3d3762db37b2a3e26e91028b0b
Xeniya Dobrogorskaya <xeniyad@Xeniyas-iMac.local>
2022-09-08 14:06:24 -0400

    React Class Components: State

--- src/App.js
+++ src/App.js
@@ -113,6 +113,31 @@ const App = () => {
     </div>
   );
 };
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchTerm: "React",
+//     };
+//   }
+//   render() {
+//     const { searchTerm } = this.state;
+//     return (
+//       <div>
+//         <h1>My Hacker Stories</h1>
+//         <SearchForm
+//           searchTerm={searchTerm}
+//           onSearchInput={() =>
+//             this.setState({
+//               searchTerm: event.target.value,
+//             })
+//           }
+//         />
+//       </div>
+//     );
+//   }
+// }
+
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
   <form onSubmit={onSearchSubmit}>
     {" "}
