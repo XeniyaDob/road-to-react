2b0a812021601b4682080275028e091bfaac6880
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2022-09-17 06:28:31 -0400

    Performance-Don’t run on first render

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import { ReactComponent as Check } from "./check.svg";
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useSemiPersistentState = (key, initialState) => {
+  const isMounted = React.useRef(false);
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
   );
 
   React.useEffect(() => {
-    localStorage.setItem(key, value);
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      console.log("A");
+      localStorage.setItem(key, value);
+    }
   }, [value, key]);
 
   return [value, setValue];
