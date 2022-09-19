7f578565aec24b4ed968a34c6bdde065923e2c7e
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2022-09-19 10:11:49 -0400

    Don’t re-render if not needed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+//Remove the strict mode. Explanation: In development mode, React’s StrictMode renders a
+//component twice to detect problems with your implementation in order to warn you about these.
 reportWebVitals();
